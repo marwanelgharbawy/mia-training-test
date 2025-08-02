a680596549e324f596e0a0b50d989f768a91a181
Marwan <marwanelgharbawy2@gmail.com>
2025-08-02 21:41:30 +0300

    Fix dividing by zero

--- calculator.py
+++ calculator.py
@@ -2,10 +2,13 @@ def add(x, y):
     return x + y
 
 def divide(x, y):
-    return x / y
+    if y != 0:
+        return x / y
+    else:
+        return None
 
 x = 10
-y = 5
+y = 0
 
 print("Sum is:", add(x, y))
 print("Quotient is:", divide(x, y))
